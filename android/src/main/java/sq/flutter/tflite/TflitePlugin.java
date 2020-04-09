c664d28d75bab185b95e607a286833a06e4a132f
brandon.maness <brandon.maness@amerch.com>
2020-04-09 15:03:03 -0500

    remove unused import

--- android/src/main/java/sq/flutter/tflite/TflitePlugin.java
+++ android/src/main/java/sq/flutter/tflite/TflitePlugin.java
@@ -28,7 +28,6 @@ import org.tensorflow.lite.Tensor;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ByteArrayOutputStream;
