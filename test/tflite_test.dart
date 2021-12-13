edc58bd2374569e3721c0708d8c9c2380ca571d7
Earvin Jake Agamata <earvin.agamata@whitecloak.com>
2021-12-13 11:11:28 +0800

    Update readme

--- test/tflite_test.dart
+++ test/tflite_test.dart
@@ -2,7 +2,7 @@ import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tflite/tflite.dart';
+import 'package:tflite_maven/tflite.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
