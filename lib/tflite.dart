9120a6fd5f62bc39ef520954f48bbdf86d802146
amirahmadadibi <amirahmadadibi@gmail.com>
2023-07-06 22:09:35 +0330

    add changes

--- lib/tflite.dart
+++ lib/tflite.dart
@@ -164,7 +164,7 @@ class Tflite {
     double imageStd = 127.5,
     double threshold = 0.1,
     int numResultsPerClass = 5,
-    int rotation: 90, // Android only
+    int rotation = 90, // Android only
     // Used in YOLO only
     List anchors = anchors,
     int blockSize = 32,
@@ -233,7 +233,7 @@ class Tflite {
     int imageWidth = 720,
     double imageMean = 0,
     double imageStd = 255.0,
-    int rotation: 90, // Android only
+    int rotation = 90, // Android only
     String outputType = "png",
     bool asynch = true,
   }) async {
@@ -319,7 +319,7 @@ class Tflite {
       int imageWidth = 720,
       double imageMean = 0,
       double imageStd = 255.0,
-      int rotation: 90, // Android only
+      int rotation = 90, // Android only
       List<int>? labelColors,
       String outputType = "png",
       bool asynch = true}) async {
@@ -385,7 +385,7 @@ class Tflite {
       int imageWidth = 720,
       double imageMean = 127.5,
       double imageStd = 127.5,
-      int rotation: 90, // Android only
+      int rotation = 90, // Android only
       int numResults = 5,
       double threshold = 0.5,
       int nmsRadius = 20,
